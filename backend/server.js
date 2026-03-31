ae15598402a2f3e839d821935463f39e5040c738
Thomasrelevant <thomas@relevantsearchmedia.com>
2026-03-31 19:41:48 +0530

    added chalk for terminal coloring

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 import app from "./src/app.js";
 import dotenv from "dotenv";
+import chalk from "chalk" ; 
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running http://localhost:${PORT}`);
+  console.log(chalk.bold.green(`Server running http://localhost:${PORT}`));
 });
 
 
--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,17 +1,18 @@
 import dns from "dns";
 import mongoose from "mongoose"
+import chalk from "chalk" ; 
 
 export const connectDB = async () => {
   try {
     dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
-    console.log("Connecting to DB...");
+    console.log(chalk.bold.yellow("Connecting to DB..."));
 
     await mongoose.connect(process.env.MONGODB_URI);
 
-    console.log(("Database Connected ✅"));
+    console.log(chalk.bold.green("Database Connected ✅"));
   } catch (error) {
-    console.error("DB Connection error: ", error);
+    console.error(chalk.bold.red("DB Connection error: ", error));
     process.exit(1);
   }
 };
